8c99b636a314da65194a317fea8732b6e2b4e50c
EdwardZZZ <zzh627@126.com>
2021-03-25 19:56:51 +0800

    feat: add isValid

--- src/Model.ts
+++ src/Model.ts
@@ -2,33 +2,35 @@ import { RulesMap, TipsMap } from './Rule';
 import { checker } from './utils';
 
 export default class Model {
-    constructor() {
-        const rules = RulesMap.get(this.constructor);
+    constructor(_?: { [key: string]: any }, isValid: boolean = true) {
+        if (isValid) {
+            const rules = RulesMap.get(this.constructor);
 
-        for (const propertyKey in rules) {
-            if ({}.hasOwnProperty.call(rules, propertyKey)) {
-                let keyValue = this[propertyKey];
+            for (const propertyKey in rules) {
+                if ({}.hasOwnProperty.call(rules, propertyKey)) {
+                    let keyValue = this[propertyKey];
 
-                Reflect.defineProperty(this, propertyKey, {
-                    enumerable: true,
-                    set(v) {
-                        const result = checker(rules[propertyKey], v);
+                    Reflect.defineProperty(this, propertyKey, {
+                        enumerable: true,
+                        set(v) {
+                            const result = checker(rules[propertyKey], v);
 
-                        if (result.length > 0) {
-                            const tips = TipsMap.get(this) || {};
+                            if (result.length > 0) {
+                                const tips = TipsMap.get(this) || {};
 
-                            tips[propertyKey] = result;
-                            TipsMap.set(this, tips);
+                                tips[propertyKey] = result;
+                                TipsMap.set(this, tips);
 
-                            return;
-                        }
+                                return;
+                            }
 
-                        keyValue = v;
-                    },
-                    get() {
-                        return keyValue;
-                    },
-                });
+                            keyValue = v;
+                        },
+                        get() {
+                            return keyValue;
+                        },
+                    });
+                }
             }
         }
     }
